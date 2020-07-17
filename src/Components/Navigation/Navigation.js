2159deab85e795cf81b6a5b88e7b63ced9fe1175
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2020-07-17 17:27:41 -0500

    -se corrigio la navegacion ahora se visualiza la animacion en chrome y firefox

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -5,34 +5,35 @@ import closeMenu from '../../assets/images/icon-close.svg'
 import Styles from './Navigation.module.css'
 class Navigation extends React.Component{
     state={
-        collapse:true
+        navStatus:'initial'
     }
 
     changeMenu=()=>{
         const prevState=this.state
-        const isShowed=prevState.collapse===false
-        this.setState({collapse:isShowed?true:false})
+        const isShowed=prevState.navStatus==='displayed'
+        this.setState({navStatus:isShowed?'closed':'displayed'})
     }
     render(){
-        const {collapse}=this.state
-        const isShowing=collapse===false
-        const menuDisplayed=isShowing?Styles.menuDisplayed:'';
+        const {navStatus}=this.state
+        const isShowing=navStatus==='displayed'
+        const menuActioned=isShowing?Styles.displayList:Styles.hideList;
+        
         return(
-            <nav className={menuDisplayed}>
+            <nav>
                 <div className={Styles.logo}>
                     <img src={logo} alt='logo'/>
                 </div>
                 
-                <ul className={Styles.list}>
+                <button className={Styles.btnMenu} onClick={this.changeMenu}>
+                    <img  src={isShowing?closeMenu:showMenu} alt='icon-menu'/>
+                </button>
+
+                <ul className={`${Styles.list} ${navStatus==='initial'?Styles.initial:menuActioned}`}>
                     <li><a href='#'>How we work</a></li>
                     <li><a href='#'>Blog</a></li>
                     <li><a href='#'>Account</a></li>
                     <li><a href='#'> <span  className={Styles.enfasis}> View Plans</span></a></li>
                 </ul>
-                
-                <button className={Styles.btnMenu} onClick={this.changeMenu}>
-                    <img  src={isShowing?closeMenu:showMenu} alt='icon-menu'/>
-                </button>
             </nav>
         )
     }
