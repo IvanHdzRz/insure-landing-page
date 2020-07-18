cabc4a4430be3cfed1e8f1a81db92b263d47099c
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2020-07-18 16:39:21 -0500

    se agregaron estilos al componenete btn action

--- src/Components/MoreInfo/MoreInfo.js
+++ src/Components/MoreInfo/MoreInfo.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Title from '../Title'
 import Styles from './MoreInfo.module.css'
+import ButtonAction from '../ButtonAction'
 const MoreInfo=()=>{
     return(
         <div className={Styles.MoreInfo}>
             <Title type='big' title='Find out more about how we work'/>
-            <button>How we work</button>
+            <ButtonAction  message='How we work'/>
             
         </div>
     );
